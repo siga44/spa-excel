c2ba9ce57fa630d52b6de4b694caff37f4bcc0d1
siga44 <seregqaaa@yandex.ru>
2020-06-09 16:33:56 +0500

    Rows and columns were added

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,25 +1,9 @@
 import {ExcelComponent} from '@core/ExcelComponent'
+import { createTable } from './table.template'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
   toHTML() {
-    return `
-    <div class="row">
-    <div class="row__info"></div>
-    <div class="row__data">
-      <div class="column">A</div>
-      <div class="column">B</div>
-      <div class="column">C</div>
-    </div>
-    </div>
-    <div class="row">
-      <div class="row__info">1</div>
-      <div class="row__data">
-        <div class="cell" contenteditable>1</div>
-        <div class="cell" contenteditable>2</div>
-        <div class="cell" contenteditable>3</div>
-      </div>
-    </div>
-    `
+    return createTable(20)
   }
 }
