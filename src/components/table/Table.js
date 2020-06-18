ec20757c8495ce4aa884377a4c1288b61d53bd0a
siga44 <seregqaaa@yandex.ru>
2020-06-18 11:56:08 +0500

    Completed resize with good perfomance

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from './table.template'
-import {$} from '@core/dom'
+import {resizeHandler} from './table.resize'
+import {shouldResize} from './table.functions'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -17,31 +18,7 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    const target = event.target.dataset.resize
-    if (target) {
-      const $resizer = $(event.target)
-      const $parent = $resizer.closest('[data-type="resizable"]')
-      const coords = $parent.getCoords()
-
-      if (target === 'col') {
-        const cols = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-
-        document.onmousemove = e => {
-          const delta = e.pageX - coords.right
-          const value = coords.width + delta
-          cols.forEach(el => $(el).css({width: value + 'px'}))
-        }
-      } else {
-        document.onmousemove = e => {
-          const delta = e.pageY - coords.bottom
-          const value = coords.height + delta
-          $parent.css({height: value + 'px'})
-        }
-      }
-
-      document.onmouseup = e => {
-        document.onmousemove = null
-      }
-    }
+    const target = shouldResize(event)
+    if (target) resizeHandler(this.$root, event, target)
   }
 }
